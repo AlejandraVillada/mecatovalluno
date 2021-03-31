cd280bcab18836f49868007d7793c047eaaa9518
DcVillamil <dcvillamil9466@gmail.com>
2021-03-31 16:37:26 -0500

    jefe produccion v1 listo

---
+++ Vista/layout/contactanos.php
@@ -0,0 +1,15 @@
+<div class="contacto">
+    <div class="contenido_contacto">
+        <div class="row">
+            <div class="col-6 texto_contacto">
+                <p>Contacto</p>
+                <p>Sedes</p>
+                <p>Direccion</p>
+                <p>Telefonos</p>
+            </div>
+            <div class="col-6">
+                <img src="../img/picadita_MCC.jpg" alt=""  class="img_contacto">
+            </div>
+        </div>
+    </div>
+</div>
--- Vista/php/view_home.php
+++ Vista/php/view_home.php
@@ -5,29 +5,9 @@ include_once ("../layout/header.php");
 
 <div class="container">
 <!-- menu -->
-    <div class="menu">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio sesion Empleados</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                </form>
-            </div>
-        </nav>
-    </div>
+<?php
+    include_once("../layout/menu_inicioEmpleados.php");
+?>
     <!-- promocion -->
     <div class="promocion" style="margin-top: 20px;">
         <div class="card">
@@ -116,21 +96,9 @@ include_once ("../layout/header.php");
         </div>
     </div>
     <!-- Contactanos -->
-    <div class="contacto">
-        <div class="contenido_contacto">
-            <div class="row">
-                <div class="col-6 texto_contacto">
-                    <p>Contacto</p>
-                    <p>Sedes</p>
-                    <p>Direccion</p>
-                    <p>Telefonos</p>
-                </div>
-                <div class="col-6">
-                    <img src="../img/picadita_MCC.jpg" alt=""  class="img_contacto">
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php
+        include_once("../layout/contactanos.php");
+    ?>
     <!-- copyright -->
     <div class="copyright">
         <h5 style="text-align: center;">@COPYRIGHT</h5>
