8b86b3cccb5881e6b8ed190c07110d858b5ca6d9
jgaviria2706 <jgaviriapasaje@gmail.com>
2021-04-01 18:33:31 -0500

    Creacion de Vistas y Modificacion de CSS

--- Vista/php/view_admin.php
+++ Vista/php/view_admin.php
@@ -1,2 +1,154 @@
 <?php include_once __DIR__."../../layout/header.php"?>
 
+<link rel="stylesheet" href="../layout/style.css">
+<?php
+include_once ("../layout/header.php");
+?>
+
+<div class="container">
+<!-- menu -->
+<?php
+    include_once ("../layout/menu_cerrarEmpleados.php");
+?>
+    <!-- acceso rapido -->
+    <div class="acceso_rapido" style="margin-top: 20px;">
+        <div class="card">
+            <div class="card-body" style="padding: 30px;">
+                <div class="row botones_acceso">
+                    <div class="col-3 texto_bienvenido2">
+                            <h1 class="card-title">Bienvenido</h1>
+                            <h4 class="card-text">Jefe de produccion</h4>
+                            <p id="nombre_empleado"></p>
+                    </div>
+                    <div class="col-3">
+                        <div class="opciones">
+                            <div class="opcion opcion1">
+                                <a href="#"></a>
+                            </div>
+                            <div class="descripcion_boton1">
+                                <p>Programacion de
+                                <br>
+                                    Produccion
+                                </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="opciones">
+                            <div class="opcion opcion2">
+                                <a href="#"></a>
+                            </div>
+                            <div class="descripcion_boton1">
+                                <p>Ingreso producto
+                                <br>
+                                    Terminado
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="opciones">
+                            <div class="opcion opcion3">
+                                <a href="#"></a>
+                            </div>
+                            <div class="descripcion_boton1">
+                                <p>Gestion Proveedores</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Botones de seccion -->
+    <div class="seccion_botones">
+        <div class="botones_acceso">
+            <div class="row linea">
+                <div class="col-2">
+                    <div class="opcion opcion4">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_boton">
+                        <p>Compra Materia
+                        <br>
+                            Prima
+                        </p>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="opcion opcion5">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_boton">
+                        <p>Gestion Inventario
+                        <br>
+                            Producto Terminado
+                        </p>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="opcion opcion6">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_boton">
+                        <p>Visualizar Nomina</p>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="opcion opcion7">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_boton">
+                        <p>Actualizacion Datos
+                        <br>
+                            Personales
+                        </p>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="opcion opcion8">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_boton">
+                        <p>Gestion Inventario
+                        <br>
+                            Materia Prima
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contactanos -->
+    <?php
+    include_once("../layout/contactanos.php");
+    ?>
+    <!-- Copyright -->
+    <div class="copyright">
+        <h5 style="text-align: center;">@COPYRIGHT</h5>
+    </div>
+</div>    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include_once ("../layout/footer.php");
+?>
+
--- Vista/php/view_asesor.php
+++ Vista/php/view_asesor.php
@@ -1,12 +1,96 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<h1>Holas</h1>
-</body>
-</html>
+<link rel="stylesheet" href="../layout/style.css">
+<?php
+include_once ("../layout/header.php");
+?>
+
+<div class="container">
+<!-- menu -->
+<?php
+    include_once ("../layout/menu_cerrarEmpleados.php");
+?>
+    <!-- acceso rapido -->
+    <div class="acceso_rapido" style="margin-top: 20px;">
+        <div class="card">
+            <div class="card-body" style="padding: 30px;">
+                <div class="row botones_acceso">
+                    <div class="col-4 texto_bienvenido">
+                            <h1 class="card-title">Bienvenido(a)</h1>
+                            <h4 class="card-text">Asesor de Ventas</h4>
+                            <p id="nombre_empleado"></p>
+                    </div>
+                    <div class="col-3">
+                        <div class="opciones">
+                            <div class="opcion opcion1">
+                                <a href="#"></a>
+                            </div>
+                            <div class="descripcion_botones2">
+                                <p>Actualizar Datos Cliente
+                                </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="opciones">
+                            <div class="opcion opcion2">
+                                <a href="#"></a>
+                            </div>
+                            <div class="descripcion_botones2">
+                                <p>Venta de Productos
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Botones de seccion -->
+    <div class="seccion_botones">
+        <div class="botones_acceso">
+            <div class="row linea">
+                <div class="col-3">
+                    <div class="opcion opcion3">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_botones">
+                        <p>Compra Materia Prima
+                        </p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="opcion opcion4">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_botones">
+                        <p>Ingreso Productos
+                        </p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="opcion opcion5">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_botones">
+                        <p>Visualizar Nómina</p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="opcion opcion6">
+                        <a href="#"></a>
+                    </div>
+                    <div class="descripcion_botones">
+                        <p>Actualización Datos
+                        <br>
+                            Personales
+                        </p>
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once ("../layout/footer.php");
+?>
