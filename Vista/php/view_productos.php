be9827fb7b756f3d7cdfa12bd74bacf7b67eee16
DcVillamil <dcvillamil9466@gmail.com>
2021-04-05 10:53:02 -0500

    seccion de produtos v1 lista

---
+++ Vista/php/view_productos.php
@@ -0,0 +1,223 @@
+<link rel="stylesheet" href="../layout/style.css">
+<?php
+include_once ("../layout/header.php");
+?>
+
+<div class="container">
+<!-- menu -->
+<?php
+    include_once ("../layout/menu_inicioEmpleados.php");
+?>
+
+<!-- seccion de productos -->
+<div class="titulo_prod">
+    <div class="alert alert-secondary" role="alert">
+        Seccion de productos a la venta
+    </div>  
+</div>
+<div class="container">
+    <div class="seccion_productos">
+        <div class="row seccion1">
+            <div class="col-6 cont_prod1">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Papa rellena</h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="card-text">Paquetes de 3 papas rellenas de arroz, huevo cocinado y guiso casero</p>
+                                <p>Costo: $15.000 Cop</p>
+                                <div class="row">
+                                    <div class="col-6">
+                                        Cantidad:<p class="cantidad">0</p>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="btn-group mas_menoscant" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-secondary">+</button>
+                                        <button type="button" class="btn btn-secondary">-</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-dark" style="margin-top: 10px;">Agregar al carrito</a>
+                            </div>
+                            <div class="col-6">
+                                <div class="imgprod1">
+                                    <a href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 cont_prod2">
+            <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Papa rellena</h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="card-text">Paquetes de 3 papas rellenas de arroz, huevo cocinado y guiso casero</p>
+                                <p>Costo: $15.000 Cop</p>
+                                <div class="row">
+                                    <div class="col-6">
+                                        Cantidad:<p class="cantidad">0</p>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="btn-group mas_menoscant" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-secondary">+</button>
+                                        <button type="button" class="btn btn-secondary">-</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-dark" style="margin-top: 10px;">Agregar al carrito</a>
+                            </div>
+                            <div class="col-6">
+                                <div class="imgprod2">
+                                    <a href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row seccion2">
+            <div class="col-6 cont_prod3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Papa rellena</h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="card-text">Paquetes de 3 papas rellenas de arroz, huevo cocinado y guiso casero</p>
+                                <p>Costo: $15.000 Cop</p>
+                                <div class="row">
+                                    <div class="col-6">
+                                        Cantidad:<p class="cantidad">0</p>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="btn-group mas_menoscant" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-secondary">+</button>
+                                        <button type="button" class="btn btn-secondary">-</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-dark" style="margin-top: 10px;">Agregar al carrito</a>
+                            </div>
+                            <div class="col-6">
+                                <div class="imgprod3">
+                                    <a href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 cont_prod4">
+            <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Papa rellena</h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="card-text">Paquetes de 3 papas rellenas de arroz, huevo cocinado y guiso casero</p>
+                                <p>Costo: $15.000 Cop</p>
+                                <div class="row">
+                                    <div class="col-6">
+                                        Cantidad:<p class="cantidad">0</p>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="btn-group mas_menoscant" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-secondary">+</button>
+                                        <button type="button" class="btn btn-secondary">-</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-dark" style="margin-top: 10px;">Agregar al carrito</a>
+                            </div>
+                            <div class="col-6">
+                                <div class="imgprod4">
+                                    <a href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row seccion3">
+            <div class="col-6 cont_prod5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Papa rellena</h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="card-text">Paquetes de 3 papas rellenas de arroz, huevo cocinado y guiso casero</p>
+                                <p>Costo: $15.000 Cop</p>
+                                <div class="row">
+                                    <div class="col-6">
+                                        Cantidad:<p class="cantidad">0</p>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="btn-group mas_menoscant" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-secondary">+</button>
+                                        <button type="button" class="btn btn-secondary">-</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-dark" style="margin-top: 10px;">Agregar al carrito</a>
+                            </div>
+                            <div class="col-6">
+                                <div class="imgprod5">
+                                    <a href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 cont_prod6">
+            <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Papa rellena</h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="card-text">Paquetes de 3 papas rellenas de arroz, huevo cocinado y guiso casero</p>
+                                <p>Costo: $15.000 Cop</p>
+                                <div class="row">
+                                    <div class="col-6">
+                                        Cantidad:<p class="cantidad">0</p>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="btn-group mas_menoscant" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-secondary">+</button>
+                                        <button type="button" class="btn btn-secondary">-</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-dark" style="margin-top: 10px;">Agregar al carrito</a>
+                            </div>
+                            <div class="col-6">
+                                <div class="imgprod6">
+                                    <a href="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<!-- Contactanos -->
+<?php
+    include_once("../layout/contactanos.php");
+    ?>
+    <!-- Copyright -->
+    <div class="copyright">
+        <h5 style="text-align: center;">@COPYRIGHT</h5>
+    </div>
+</div>    
