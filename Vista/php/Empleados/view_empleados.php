c0d87a2ccbc87a0f90214e0cacc6116d9a7c39c6
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2021-04-20 18:44:15 -0500

    vistas

--- Vista/php/Empleados/view_empleados.php
+++ Vista/php/Empleados/view_empleados.php
@@ -18,21 +18,7 @@
             <li><a href="#">Gestión de Paises</a></li>
             <li><a href="#">Gestión de sedes</a></li>
 
-            <!--<li class="dropdown">
-       <a href="#works" class="dropdown-toggle"  data-toggle="dropdown">Works <span class="caret"></span></a>
-     <ul class="dropdown-menu animated fadeInLeft" role="menu">
-      <div class="dropdown-header">Dropdown heading</div>
-      <li><a href="#pictures">Pictures</a></li>
-      <li><a href="#videos">Videeos</a></li>
-      <li><a href="#books">Books</a></li>
-      <li><a href="#art">Art</a></li>
-      <li><a href="#awards">Awards</a></li>
-      </ul>
-      </li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#contact">Contact</a></li>
-      <li><a href="#followme">Follow me</a></li>
-      </ul>-->
+           
     </nav>
     <!-- /#sidebar-wrapper -->
 
