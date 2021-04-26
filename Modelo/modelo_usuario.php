f2f6d087a9d384893082835f2d097b468f265139
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2021-04-26 11:04:37 -0500

    mod

--- Modelo/modelo_usuario.php
+++ Modelo/modelo_usuario.php
@@ -28,10 +28,10 @@ class modelo_usuario extends ModeloAbstractoDB{
 			if(count($this->rows) == 1):
 				foreach ($this->rows[0] as $propiedad=>$valor):
 					 $this->$propiedad = $valor;
-	              echo $propiedad;
+	            //  echo $propiedad;
 				endforeach;
 			endif;
-	    var_dump($this->rows);
+	    return($this->rows);
 
     }
 
