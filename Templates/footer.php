8cb8cefb5268e2b83e4071c403c05bbb4128c3f8
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2021-05-01 11:14:55 -0500

    headeracomodado

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,43 +1,51 @@
   <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+      <div class="pull-right hidden-xs">
+          <b>Version</b> 2.4.0
+      </div>
+      <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+      reserved.
   </footer>
 
   </div>
-<!-- ./wrapper -->
+  <!-- ./wrapper -->
 
-<!-- jQuery 3 -->
-<script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  <script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"
-  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-  crossorigin="anonymous"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="./Recursos/js/bootstrap.min.js"></script>
-<!-- SlimScroll -->
-<script src="./Recursos/js/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="./Recursos/js/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="./Recursos/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="./Recursos/js/demo.js"></script>
-<script src="./Recursos/js/funcionesJquery.js"></script>
+  <!-- jQuery 3 -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="./Recursos/js/bootstrap.min.js"></script>
+  <!-- SlimScroll -->
+  <script src="./Recursos/js/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="./Recursos/js/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="./Recursos/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="./Recursos/js/demo.js"></script>
+  <script src="./Recursos/js/funcionesJquery.js"></script>
 
-<script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.11/js/dataTables.bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.2/sweetalert2.all.js"></script>
+  <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.11/js/dataTables.bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.2/sweetalert2.all.js"></script>
 
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 
-<script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js">
+  </script>
+
+  <script>
   $(document).ready(Inicio);
-</script>
-</body>
-</html>
+  </script>
+  </body>
+
+  </html>
