344ffe6337ae4755aa3cf8343dc61027bda1ada0
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2021-05-19 13:45:20 -0500

    correct home

--- Recursos/js/home_productos/home_productos.js
+++ Recursos/js/home_productos/home_productos.js
@@ -6,7 +6,7 @@ function productos() {
         dataType: "json"
     }).done(function(resultado) {
         $.each(resultado.data, function(index, value) {
-            $("#menu .menu-container").append("<div class='col-lg-6 menu-item'>" + value.Foto +
+            $("#menu .menu-container").append("<div class='col-lg-6 menu-item filter-starters'>" + value.Foto +
                 "<div class = 'menu-content'><a href = '#'>" + value.NombreProducto + "</a><span>" + value.ValorUnitario + "</span ></div></div>")
         });
     });
